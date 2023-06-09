257ce316a680117560dec6c4e3d627fe0ed24fc5
Vitovszki Tamas <lolozol80xd@gmail.com>
2023-06-09 09:46:46 +0200

    phone app ongoing

--- App.js
+++ App.js
@@ -1,10 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Button } from 'react-native-web';
 
 export default function App() {
+ const [count,setCount]= useState(0)
+  function increment(){
+    setCount(count+1)
+  };
+  function reset(){
+    setCount(0)
+  }
+  function decrease(){
+    setCount(count-1)
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>Számláló</Text>
+      <Text style={styles.count}>{count}</Text>
+      <Button title="Növel"
+      onPress={increment}/>
+      <Button title="Csökkent"
+      onPress={decrease}/>
+      <Button title="Reset"
+      onPress={reset}/>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +32,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'grey',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  title:{
+    color:'white',
+    fontSize:32,
+  },
+  count:{
+    color:'yellow',
+    fontSize:44,
+    alignItems: 'center',
   },
 });
